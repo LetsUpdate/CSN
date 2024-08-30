fdc29c1b2128f730f3279dde6d3b1c1e4a91aedd
RED <example.com>
2024-08-30 15:46:51 +0200

    fixing that shadow bug, now the script is near perfect!

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 const utils = require("./utils");
 const { LookForCaptcha } = require('./captchaSolver');
 
+
+
 (function () {
     'use strict';
     console.log("Hello from main.user.js    ");
+
+
     if (utils.isNeptunPage() && utils.isLoginPage()) {
         // Set up a Mutation Observer to watch for changes in the DOM
         console.log("LookForCaptcha");
