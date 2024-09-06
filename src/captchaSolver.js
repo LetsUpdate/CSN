769994b8e7f283559937b0663ebef15e60bce4d9
RED <example.com>
2024-09-06 13:03:27 +0200

    version 1.0.3

--- src/captchaSolver.js
+++ src/captchaSolver.js
@@ -1,7 +1,7 @@
-const {solveCaptcha} = require('./captchaSolverApi');
-const {SetState, LogoState} = require('./UImodul');
+const { solveCaptcha } = require('./captchaSolverApi');
+const { SetState, LogoState } = require('./UImodul');
 
-const $ = window.jQuery; 
+const $ = window.jQuery;
 
 //<img class="captchaRefreshIcon" src="App_Themes/Common_Images/refresh.svg" alt="refreshIcon">
 function getCaptchaRefreshIcon() {
@@ -19,7 +19,7 @@ function getCaptchaInput() {
 //body > div.ui-dialog.ui-widget.ui-widget-content.ui-corner-all.ui-front.ui-dialog-buttons.ui-draggable > div.ui-dialog-buttonpane.ui-widget-content.ui-helper-clearfix > div > button:nth-child(2)
 function getNewLoginButton() {
     // Use a more specific selector to target buttons with text containing 'Belépés' or 'login' (case-insensitive)
-    return $("button.ui-button").filter(function() {
+    return $("button.ui-button").filter(function () {
         return /belépés|login/i.test($(this).text());
     });
 }
@@ -38,7 +38,7 @@ function scanForElementText(element, timeout = 10000) {
         let elapsedTime = 0;
 
         const interval = setInterval(() => {
-            if(element===null) {return;}
+            if (element === null) { return; }
             if (element.text().trim().length > 0) {
                 clearInterval(interval);
                 resolve(false); // Captcha is invalid
@@ -52,10 +52,10 @@ function scanForElementText(element, timeout = 10000) {
         }, intervalTime);
     });
 }
-let tryes =0;
+let tryes = 0;
 async function StartSolving(imgsrc) {
-   
-    if(tryes>4){
+
+    if (tryes > 4) {
         SetState(LogoState.error);
         return;
     }
@@ -68,66 +68,69 @@ async function StartSolving(imgsrc) {
     const captchaInput = getCaptchaInput()
     const captchaRefreshIcon = getCaptchaRefreshIcon();
 
-        try {
-            console.log('Solving captcha...');
-            await new Promise((resolve) => setTimeout(resolve, 100));
-            let captchaSolution;
-            if(imgsrc === captchaImage[0].src){
-            captchaSolution= await solveCaptcha(captchaImage[0]);
-         }
-            
-            
-            captchaInput.val(captchaSolution);
+    try {
+        console.log('Solving captcha...');
+        await new Promise((resolve) => setTimeout(resolve, 100));
+        let captchaSolution;
+        if (imgsrc === captchaImage[0].src) {
+            captchaSolution = await solveCaptcha(captchaImage[0]);
+        } else {
+            console.log('Captcha image changed, terminating...');
+            return;
+        }
 
-            if(imgsrc === captchaImage[0].src) {
+
+        captchaInput.val(captchaSolution);
+
+        if (imgsrc === captchaImage[0].src) {
             await new Promise((resolve) => setTimeout(resolve, 100));
             loginButton.click();
             console.log('Captcha solved:', captchaSolution);
-            }else {
-                console.log('Captcha image changed, solving again...');
-                return;
-            }
+        } else {
+            console.log('Captcha image changed, terminating...');
+            return;
+        }
 
-            // Ne spammeljük a szervert szét
-            await new Promise((resolve) => setTimeout(resolve, 500));
-        
-
-            //Mutotion observer observe the error label
-            //if the captcha is invalid the error label will not be empty
+        // Ne spammeljük a szervert szét
+        await new Promise((resolve) => setTimeout(resolve, 500));
 
 
-            //wait for the error label to appear
-            console.log('Waiting for error label...');
-            try {
-                await scanForElementText(getErrorLabel());
-                console.log('Captcha is invalid:', getErrorLabel().text());
-                getErrorLabel().text("");  
-                console.log('waitforCaptchaRefresh');
-                captchaRefreshIcon.click();
-                return;
-            }
-            catch (error) {
-                if(error === "TIME_OUT") {
-                    console.log('TimedOut');
-                }else {
-                    console.error('Failed to solve captcha:', error);
-                }
-                return;
-            }
+        //Mutotion observer observe the error label
+        //if the captcha is invalid the error label will not be empty
 
+
+        //wait for the error label to appear
+        console.log('Waiting for error label...');
+        try {
+            await scanForElementText(getErrorLabel());
+            console.log('Captcha is invalid:', getErrorLabel().text());
+            getErrorLabel().text("");
+            console.log('waitforCaptchaRefresh');
+            captchaRefreshIcon.click();
+            return;
         }
         catch (error) {
+            if (error === "TIME_OUT") {
+                console.log('TimedOut');
+            } else {
+                console.error('Failed to solve captcha:', error);
+            }
+            return;
+        }
 
-            if(error === "HARD_IMAGE") {
-                captchaRefreshIcon.click();
-             
-            }else {
-            
+    }
+    catch (error) {
+
+        if (error === "HARD_IMAGE") {
+            captchaRefreshIcon.click();
+
+        } else {
+
             console.error('Failed to solve captcha:', error);
             SetState(LogoState.networkError);
             return;
-            } 
         }
+    }
 }
 
 
@@ -146,51 +149,48 @@ function LookForCaptcha() {
     const imgElement = $imgElement[0];
 
     //onbutton click reset tryes
-    getFirstLogin().click(function() {
-        tryes=0;
+    getFirstLogin().click(function () {
+        tryes = 0;
     });
 
-// Check if the element exists
-if (imgElement) {
-    // Create a new MutationObserver
-    const observer = new MutationObserver((mutations) => {
-        mutations.forEach((mutation) => {
-            if (mutation.type === 'attributes' && mutation.attributeName === 'src') {
-                console.log('Image src changed to:', imgElement.src);
-                
+    // Check if the element exists
+    if (imgElement) {
+        // Create a new MutationObserver
+        const observer = new MutationObserver((mutations) => {
+            mutations.forEach((mutation) => {
+                if (mutation.type === 'attributes' && mutation.attributeName === 'src') {
+                    console.log('Image src changed to:', imgElement.src);
 
-                // Attach a load event listener to ensure the image is loaded
-                $imgElement.off('load').on('load', function() {
-                    if (this.complete && this.naturalWidth > 0) {
-                        handleImageLoad();
-                    }
-                });
 
-                // Also handle the case where the image might already be cached
-                // if (imgElement.complete && imgElement.naturalWidth > 0) {
-                //     console.log('Image loaded from cache:', imgElement.src);
-                //     handleImageLoad();
-                // }
-                const $abortLoginLink = $('#abortLogin > a');
-                if ($abortLoginLink.length) {
-                    $abortLoginLink.click();
+                    // Attach a load event listener to ensure the image is loaded
+                    $imgElement.off('load').on('load', function () {
+                        if (this.complete && this.naturalWidth > 0) {
+                            handleImageLoad();
+                        }
+                    });
+
+                    // Also handle the case where the image might already be cached
+                    // if (imgElement.complete && imgElement.naturalWidth > 0) {
+                    //     console.log('Image loaded from cache:', imgElement.src);
+                    //     handleImageLoad();
+                    // }
+
+
                 }
-
-            }
+            });
         });
-    });
 
-    // Start observing the image element for changes in its attributes
-    observer.observe(imgElement, {
-        attributes: true // Listen for attribute changes
-    });
-} else {
-    console.error('Image element not found');
-}
+        // Start observing the image element for changes in its attributes
+        observer.observe(imgElement, {
+            attributes: true // Listen for attribute changes
+        });
+    } else {
+        console.error('Image element not found');
+    }
 
     console.log('Observing document body for changes...');
 }
 
 
 
-module.exports = { LookForCaptcha};
+module.exports = { LookForCaptcha };
--- src/index.js
+++ src/index.js
@@ -9,6 +9,16 @@ const {PlaceLogo} = require('./UImodul');
 
     if (utils.isNeptunPage() && utils.isLoginPage()) {
         // Set up a Mutation Observer to watch for changes in the DOM
+
+        //Prevent npu to press login after the user too
+        document.addEventListener('keydown', function(event) {
+            if (event.key === 'Enter') {
+                const $abortLoginLink = $('#abortLogin > a');
+                if ($abortLoginLink.length) {
+                    $abortLoginLink[0].click();
+                }
+            }
+        });
         console.log("LookForCaptcha");
         PlaceLogo();
         LookForCaptcha();
