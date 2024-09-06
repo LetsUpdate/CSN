f3a21da6f7fe38a96c4bfd49d09c79ce5e414556
RED <example.com>
2024-09-06 12:23:13 +0200

    URL update

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 const config = {
-    API_URL: 'https://csn.letsdie.party/CSN',
+    API_URL: 'https://csn.redsparks.eu/api/v1/solve',
 }
-
+//config.API_URL = 'http://localhost:8000/api/v1/solve';
 module.exports = config;
